9cf79067cbabf226300539388727f8a257037c53
Jonathan Ma <majo@oregonstate.edu>
2021-05-30 21:49:29 -0700

    Implemented check 400 method that checks if the year input is divisible by 400 and if it is then it would return ___ is a leap year

--- leapYear.py
+++ leapYear.py
@@ -1,5 +1,7 @@
 def LeapYear(year):
-    if (year % 100 == 0 ):
+    if ( year % 400 == 0):
+        print(year, "is a leap year.")
+    elif (year % 100 == 0 ):
         print(year, "is not a leap year.")
     elif (year % 4 == 0):
         print(year, "is a leap year.")
