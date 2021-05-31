0d71b07e70c6981e6de50f5d27e73a357623bfe2
Jonathan Ma <majo@oregonstate.edu>
2021-05-30 21:30:23 -0700

    Wrote first test, to see if number entered (2012) in the program outputs the message '2012 is a leap year.'

---
+++ test_leapYear.py
@@ -0,0 +1,17 @@
+import unittest
+from unittest.mock import patch
+import leapYear
+from io import StringIO
+
+class Test_LeapYear(unittest.TestCase):
+    # Test first condition that if a number is evenly divisible by 4
+    # THen it is a leap year
+    def test_isLeapYear4(self):
+        with patch('sys.stdout', new = StringIO()) as printedMsg:
+            yearInput = 2012
+            leapYear.LeapYear(2012)
+            result = (printedMsg.getvalue()).replace('\n','')
+            self.assertEqual(result, "2012 is a leap year.")
+
+if __name__ == '__main__':
+    unittest.main()
