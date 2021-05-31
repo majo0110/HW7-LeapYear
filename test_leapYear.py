af8f1a88bff25dd38c24059611e633cb4b5011be
Jonathan Ma <majo@oregonstate.edu>
2021-05-30 21:39:03 -0700

    Wrote second test, input 2100 and test to see if it outputs 2100 is not a leap year b/c its a multiple of 100

--- test_leapYear.py
+++ test_leapYear.py
@@ -13,5 +13,12 @@ class Test_LeapYear(unittest.TestCase):
             result = (printedMsg.getvalue()).replace('\n','')
             self.assertEqual(result, "2012 is a leap year.")
 
+    def test_notLeapYear_4and100(self):
+        with patch('sys.stdout', new = StringIO()) as printedMsg:
+            yearInput = 2100
+            leapYear.LeapYear(2100)
+            result = (printedMsg.getvalue()).replace('\n','')
+            self.assertEqual(result, "2100 is not a leap year.")
+
 if __name__ == '__main__':
     unittest.main()
